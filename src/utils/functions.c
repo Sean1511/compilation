dfb77e179c6b027a6e51541aa26dc21ae50b19c8
Sean1511 <sean@DESKTOP-17AKS3Q.localdomain>
2019-12-05 18:24:35 +0200

    initial

--- src/utils/functions.c
+++ src/utils/functions.c
@@ -1,47 +1,49 @@
 #include "functions.h"
+#include <stddef.h>
 
-void printtree (node *tree, int tab){
-    int i; 
-    char* token = tree->token;
-   
-    if (*token){
-         for (i = 0; i< tab; i++){
-            printf ("\t");
-        }
-        printf ("(%s\n", token);
-    }
-    else
-        tab -= 1;
-    if (tree -> left){
-        printtree (tree-> left, tab + 1);
-    }
-
-    if (tree -> middle_l){
-        printtree (tree-> middle_l, tab + 1);
-    }
-
-    if (tree -> middle_r){
-        printtree (tree-> middle_r, tab + 1);
-    }
-          
-    if (tree -> right){
-        printtree (tree-> right, tab + 1); 
-    }
-    for (i = 0; i< tab; i++){
-        printf ("\t");
-    }
-    if (*token)
-        printf(")\n");
+void addNode(node** father, node* son) {
+	if ((*father)->count == 0) {
+		(*father)->nodes = (node**)malloc(sizeof(node*));
+		(*father)->nodes[(*father)->count] = son;
+		(*father)->count++;
+	}
+	else {
+		(*father)->nodes = (node**)realloc((*father)->nodes, ((*father)->count+1)*sizeof(node*));
+		(*father)->nodes[(*father)->count] = son;
+		(*father)->count++;
+	}
 }
 
-node *mknode (char *token, node *left, node *middle_l, node *middle_r, node *right){
-    node *newnode = (node*)malloc (sizeof(node));
-    char *newstr = (char*)malloc (sizeof(token)+1);
-    strcpy (newstr, token);
-    newnode->left = left;
-    newnode->right = right;
-    newnode->middle_l = middle_l;
-    newnode->middle_r = middle_r;
-    newnode->token = newstr;
-    return newnode;
+void printtree(node* tree, int tab) {
+	int i;
+	char* token = tree->token;
+
+	if (*token) {
+		for (i = 0; i < tab; i++) {
+			printf("\t");
+		}
+		printf("(%s\n", token);
+	}
+	else
+		tab -= 1;
+	if (tree->nodes) {
+		for (int j = 0; j < tree->count; j++) {
+			printtree(tree->nodes[j], tab + 1);
+		}
+	}
+	for (i = 0; i < tab; i++) {
+		printf("\t");
+	}
+	if (*token)
+		printf(")\n");
+}
+
+node* mknode(char* token) {
+	node *newnode = (node*)malloc(sizeof(node));
+	char *newstr = (char*)malloc(sizeof(token) + 1);
+	strcpy(newstr, token);
+	newnode->token = newstr;
+	newnode->nodes = NULL;
+	newnode->count = 0;
+	return newnode;
 }
--- src/utils/functions.h
+++ src/utils/functions.h
@@ -1,15 +1,14 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-typedef struct node{
-    char *token;
-    struct node *left;
-    struct node *middle_l;
-    struct node *middle_r;
-    struct node *right;
+typedef struct node {
+	char* token;
+	int count;
+	struct node** nodes;
 } node;
 
-node *mknode (char *token, node *left, node *middle_l, node *middle_r, node *right);
-void printtree (node *tree, int tab); 
+node* mknode(char* token);
+void addNode(node** father, node* son);
+void printtree(node* tree, int tab);
 
 #endif 
